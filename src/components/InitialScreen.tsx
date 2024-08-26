d5e9f617eb5513f1e7a9de725aea171834d70263
seancze <60085582+seancze@users.noreply.github.com>
2024-08-26 19:50:39 +0800

    feat: reduce padding in initial screen

--- src/components/InitialScreen.tsx
+++ src/components/InitialScreen.tsx
@@ -22,7 +22,7 @@ export const InitialScreen: React.FC<InitialScreenProps> = ({
     <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-between sm:py-12">
       <div className="relative py-3 sm:max-w-3xl sm:mx-auto w-full">
         <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
-        <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+        <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-12">
           <div className="max-w-3xl mx-auto">
             <Logo />
             <Description />
