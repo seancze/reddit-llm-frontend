04b91baacd416c54e669efa6b767a50ffb697b07
seancze <60085582+seancze@users.noreply.github.com>
2024-08-30 15:25:55 +0800

    feat: add hotjar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/components/AuthProvider";
 import { auth } from "@/auth";
+import { HotjarInitialiser } from "@/components/HotjarInitialiser";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default async function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <AuthProvider session={session}>{children}</AuthProvider>
+        <HotjarInitialiser />
       </body>
     </html>
   );
