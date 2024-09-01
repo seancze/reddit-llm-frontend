c173eaea891515677ad1bd0b0806e335bf588e5b
seancze <60085582+seancze@users.noreply.github.com>
2024-09-01 15:14:07 +0800

    feat: reduce length of description + add login hyperlink

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -1,14 +1,33 @@
-export const Description = () => {
+import { useSession, signIn } from "next-auth/react";
+
+export const Description: React.FC = () => {
+  const { data: session } = useSession();
+
   return (
     <div className="text-center mb-8">
       <h1 className="text-2xl font-bold mb-2 text-white">
         Your reddit questions, answered
       </h1>
       <p className="text-gray-300">
-        Click one of our example questions below or login to ask your own
-        questions.
-        <br />
-        Supported subreddit(s): r/SGExams
+        Click one of our questions below
+        {!session ? (
+          <>
+            {" or "}
+            <a
+              href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                signIn("reddit");
+              }}
+              className="text-cyan-500 hover:text-cyan-400 underline"
+            >
+              login with Reddit
+            </a>
+            {" to ask your own question"}
+          </>
+        ) : (
+          " or ask your own question"
+        )}
       </p>
     </div>
   );
