d177e9f99f278c5c4a8e1a5266db10cfcbd06ba7
seancze <60085582+seancze@users.noreply.github.com>
2024-09-02 16:58:50 +0800

    refactor: use dictionary to hold example questions
    This commit allows each example question to be mapped to a hardcoded query id
    so that each example question can be cached permanently.

---
+++ src/app/utils/constants.ts
@@ -0,0 +1,11 @@
+type QuestionsDict = {
+  [key: string]: string;
+};
+
+export const questionsDict: QuestionsDict = {
+  "what are the key challenges that youths face?": "66d44daf598fba558c868f1a",
+  "group all posts by category in descending order and provide a % breakdown":
+    "66d4730fb71154382575eef8",
+  "what are the top 3 most popular posts?": "66d43353b0d46599ed963670",
+  "what are some time management tips?": "66d44d905e53e4575d513fdc",
+};
--- src/components/ExampleQuestions.tsx
+++ src/components/ExampleQuestions.tsx
@@ -1,30 +1,31 @@
 import React from "react";
+import { questionsDict } from "@/app/utils/constants";
 
 interface ExampleQuestionsProps {
   onQuestionClick: (question: string) => void;
 }
 
+const capitalizeFirstLetter = (str: string): string => {
+  return str.length > 0 ? str.charAt(0).toUpperCase() + str.slice(1) : str;
+};
+
 export const ExampleQuestions: React.FC<ExampleQuestionsProps> = ({
   onQuestionClick,
 }) => {
-  const questions = [
-    "What are the key challenges that youths face?",
-    "Group all posts by category in descending order and provide a % breakdown",
-    "What are the top 3 most popular posts?",
-    "What are some time management tips?",
-  ];
-
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-2 mb-8">
-      {questions.map((q, index) => (
-        <button
-          key={index}
-          className="bg-gray-700 hover:bg-gray-600 rounded-lg p-2 text-xs md:text-sm text-left text-white border border-gray-600 hover:border-gray-400 transition-all duration-200 ease-in-out transform hover:-translate-y-0.5 focus:outline-none focus:ring-2 focus:ring-gray-400"
-          onClick={() => onQuestionClick(q)}
-        >
-          {q}
-        </button>
-      ))}
+      {Object.keys(questionsDict).map((question, index) => {
+        const questionFormatted = capitalizeFirstLetter(question);
+        return (
+          <button
+            key={index}
+            className="bg-gray-700 hover:bg-gray-600 rounded-lg p-2 text-xs md:text-sm text-left text-white border border-gray-600 hover:border-gray-400 transition-all duration-200 ease-in-out transform hover:-translate-y-0.5 focus:outline-none focus:ring-2 focus:ring-gray-400"
+            onClick={() => onQuestionClick(questionFormatted)}
+          >
+            {capitalizeFirstLetter(questionFormatted)}
+          </button>
+        );
+      })}
     </div>
   );
 };
