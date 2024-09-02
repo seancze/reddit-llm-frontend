b91d80fa2bad9e75aafe6841cfc325c336b16201
seancze <60085582+seancze@users.noreply.github.com>
2024-09-02 16:57:47 +0800

    feat: create custom jwt so that backend can verify authenticated requests

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,46 @@
 import NextAuth from "next-auth";
 import Reddit from "next-auth/providers/reddit";
+import type { NextAuthConfig } from "next-auth";
+import jwt from "jsonwebtoken";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
+const ALGORITHM = "HS256";
+const ACCESS_TOKEN_EXPIRE_MINUTES = 60;
+
+const createJWT = (username: string | null | undefined) => {
+  if (!username) return "";
+
+  const expirationTime =
+    Math.floor(Date.now() / 1000) + ACCESS_TOKEN_EXPIRE_MINUTES * 60;
+
+  const payload = {
+    username,
+    exp: expirationTime,
+  };
+
+  return jwt.sign(payload, process.env.AUTH_SECRET!, { algorithm: ALGORITHM });
+};
+
+export const config = {
   providers: [Reddit],
-});
+  callbacks: {
+    async session({ session, token }) {
+      const jwt = createJWT(session.user.name);
+      // send properties to the client, like an access_token from a provider.
+      return {
+        ...session,
+        accessToken: token.accessToken,
+        jwt,
+      };
+    },
+  },
+} satisfies NextAuthConfig;
+
+export const { handlers, signIn, signOut, auth } = NextAuth(config);
+
+// extend the built-in session type
+declare module "next-auth" {
+  interface Session {
+    accessToken?: string;
+    jwt?: string;
+  }
+}
