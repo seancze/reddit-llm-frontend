c2d50e645cee4b310e1a4148afc61c59eb848c43
seancze <60085582+seancze@users.noreply.github.com>
2024-09-14 15:50:16 +0800

    feat: add util function to convert date string to seconds since epoch

--- src/app/utils/format.ts
+++ src/app/utils/format.ts
@@ -1,3 +1,9 @@
 export const capitaliseFirstLetter = (str: string): string => {
   return str.length > 0 ? str.charAt(0).toUpperCase() + str.slice(1) : str;
 };
+
+// converts a date string formatted as "YYYY-MM-DDTHH:MM:SSZ" to seconds since epoch
+export const toSecondsSinceEpoch = (dateString: string) => {
+  const date = new Date(dateString);
+  return Math.floor(date.getTime() / 1000);
+};
