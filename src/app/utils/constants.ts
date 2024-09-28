9663bf60f6bc884ed46953f999203f8cd1836cca
seancze <60085582+seancze@users.noreply.github.com>
2024-09-28 17:46:31 +0800

    feat: update id of example question

--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -8,7 +8,7 @@ export const questionsDict: QuestionsDict = {
   "66d44daf598fba558c868f1a": "What are the key challenges that youths face?",
   "66d4730fb71154382575eef8":
     "Group all posts by category in descending order and provide a % breakdown",
-  "66d43353b0d46599ed963670": "What are the top 3 most popular posts?",
+  "66f7cb23aa0e750df917b2ee": "What are the top 3 most popular posts?",
   "66d44d905e53e4575d513fdc": "What are some time management tips?",
 };
 
