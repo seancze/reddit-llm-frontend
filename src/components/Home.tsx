1328df8c63f6e88c61ef61928720cb86ca2a2373
seancze <60085582+seancze@users.noreply.github.com>
2024-09-28 16:52:18 +0800

    refactor: make use of useContext for global state

---
+++ src/components/Home.tsx
@@ -0,0 +1,174 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { Message } from "@/types/message";
+import { InitialScreen } from "@/components/InitialScreen";
+import { ChatInterface } from "@/components/ChatInterface";
+import { Header } from "@/components/Header";
+import { useSession } from "next-auth/react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { toastConfig } from "@/app/utils/constants";
+import { FaSpinner } from "react-icons/fa";
+import { useChatContext } from "@/contexts/ChatContext";
+import { ChatData } from "@/types/chatData";
+
+export const Home = ({
+  initialChatData,
+  initialError,
+}: {
+  initialChatData?: ChatData;
+  initialError?: string;
+}) => {
+  const {
+    messages,
+    setMessages,
+    queryId,
+    setQueryId,
+    chatId,
+    setChatId,
+    isChatOwner,
+    setIsChatOwner,
+    currentVote,
+    setCurrentVote,
+    isGettingChat,
+  } = useChatContext();
+  const [isLoading, setIsLoading] = useState(false);
+  const { data: session } = useSession();
+
+  console.log({ session });
+
+  useEffect(() => {
+    if (initialChatData) {
+      setMessages(initialChatData.messages);
+      setQueryId(initialChatData.lastQueryId);
+      setChatId(initialChatData.chatId);
+      setIsChatOwner(initialChatData.isChatOwner);
+      setCurrentVote(initialChatData.vote);
+    }
+  }, [
+    initialChatData,
+    setMessages,
+    setQueryId,
+    setChatId,
+    setIsChatOwner,
+    setCurrentVote,
+  ]);
+
+  useEffect(() => {
+    if (initialError) {
+      toast.error(initialError, toastConfig);
+    }
+  }, [initialError]);
+
+  // TODO: refactor this function
+  const handleSendMessage = async (message: string) => {
+    setIsLoading(true);
+    const messageWithQuestion: Message[] = [
+      ...messages,
+      { role: "user", content: message },
+    ];
+    setMessages(messageWithQuestion);
+
+    try {
+      let data;
+      let response;
+      // if the user is not logged in, the user should only be able to get cached results
+      if (!session) {
+        response = await fetch(
+          `${process.env.NEXT_PUBLIC_BACKEND_DOMAIN}chat/${message}`,
+          {
+            method: "GET",
+          }
+        );
+      } else {
+        response = await fetch(
+          `${process.env.NEXT_PUBLIC_BACKEND_DOMAIN}query`,
+          {
+            method: "POST",
+            headers: {
+              Authorization: `Bearer ${session?.jwt}`,
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              query: messageWithQuestion,
+              username: session?.user?.name,
+              chat_id: chatId,
+            }),
+          }
+        );
+      }
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+
+      data = await response.json();
+      console.log({ data });
+      if (!data || !data.response) {
+        throw new Error("Invalid response from server");
+      }
+
+      const lastIndex = data.response.length - 1;
+
+      setMessages([...messageWithQuestion, data.response[lastIndex]]);
+      setQueryId(data.query_id);
+      setChatId(data.chat_id);
+      setCurrentVote(data.user_vote);
+      setIsChatOwner(true);
+    } catch (error) {
+      console.log({ errorFetchingResponse: error });
+      let errorMessage =
+        error instanceof Error ? error.message : "An unexpected error occurred";
+      if (errorMessage === "Unauthorized") {
+        if (session) {
+          errorMessage = "Your session has expired. Please login again";
+        } else {
+          errorMessage = "Unauthorised: Please login to continue";
+        }
+      }
+      toast.error(errorMessage, toastConfig);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleBackClick = () => {
+    setMessages([]);
+    setQueryId("");
+    setChatId("");
+    setIsChatOwner(true);
+  };
+
+  return (
+    <div className="flex flex-col h-screen bg-gray-900 text-white">
+      <Header />
+      <main className="flex-grow overflow-hidden">
+        {isGettingChat ? (
+          <div className="flex justify-center items-center my-4">
+            <FaSpinner className="animate-spin text-cyan-500 text-4xl" />
+          </div>
+        ) : messages.length === 0 ? (
+          <InitialScreen
+            onQuestionClick={handleSendMessage}
+            onSendMessage={handleSendMessage}
+            isLoading={isLoading}
+            isChatOwner={isChatOwner}
+          />
+        ) : (
+          <ChatInterface
+            queryId={queryId}
+            chatId={chatId}
+            messages={messages}
+            isLoading={isLoading}
+            onBackClick={handleBackClick}
+            onSendMessage={handleSendMessage}
+            currentVote={currentVote}
+            setCurrentVote={setCurrentVote}
+            isChatOwner={isChatOwner}
+          />
+        )}
+      </main>
+      <ToastContainer theme="dark" />
+    </div>
+  );
+};
---
+++ src/contexts/ChatContext.tsx
@@ -0,0 +1,67 @@
+// contexts/ChatContext.tsx
+"use client";
+
+import React, { createContext, useContext, useState } from "react";
+import { Message } from "@/types/message";
+
+interface ChatContextType {
+  messages: Message[];
+  setMessages: React.Dispatch<React.SetStateAction<Message[]>>;
+  isLoading: boolean;
+  setIsLoading: React.Dispatch<React.SetStateAction<boolean>>;
+  queryId: string;
+  setQueryId: React.Dispatch<React.SetStateAction<string>>;
+  chatId: string;
+  setChatId: React.Dispatch<React.SetStateAction<string>>;
+  isChatOwner: boolean;
+  setIsChatOwner: React.Dispatch<React.SetStateAction<boolean>>;
+  currentVote: -1 | 0 | 1;
+  setCurrentVote: React.Dispatch<React.SetStateAction<-1 | 0 | 1>>;
+  isGettingChat: boolean;
+  setIsGettingChat: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const ChatContext = createContext<ChatContextType | undefined>(undefined);
+
+export const ChatProvider: React.FC<{
+  children: React.ReactNode;
+}> = ({ children }) => {
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [queryId, setQueryId] = useState("");
+  const [chatId, setChatId] = useState("");
+  const [isChatOwner, setIsChatOwner] = useState(true);
+  const [currentVote, setCurrentVote] = useState<-1 | 0 | 1>(0);
+  const [isGettingChat, setIsGettingChat] = useState(false);
+
+  return (
+    <ChatContext.Provider
+      value={{
+        messages,
+        setMessages,
+        isLoading,
+        setIsLoading,
+        queryId,
+        setQueryId,
+        chatId,
+        setChatId,
+        isChatOwner,
+        setIsChatOwner,
+        currentVote,
+        setCurrentVote,
+        isGettingChat,
+        setIsGettingChat,
+      }}
+    >
+      {children}
+    </ChatContext.Provider>
+  );
+};
+
+export const useChatContext = () => {
+  const context = useContext(ChatContext);
+  if (context === undefined) {
+    throw new Error("useChatContext must be used within a ChatProvider");
+  }
+  return context;
+};
