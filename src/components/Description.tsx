5165ad0b74d3d668d4819b9e02920c8f18a2d04e
Sean <seancze@gmail.com>
2025-05-31 14:48:01 +0800

    feat: update font size of description component

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -5,10 +5,10 @@ export const Description: React.FC = () => {
 
   return (
     <div className="text-center">
-      <h1 className="text-2xl font-bold mb-2">
+      <h1 className="text-xl sm:text-3xl md:text-4xl font-bold mb-2">
         Your Reddit questions, answered
       </h1>
-      <p>
+      <p className="text-sm sm:text-lg md:text-xl">
         Click a question below
         {!session ? (
           <>
